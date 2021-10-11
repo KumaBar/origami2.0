ada2876b4b520254fcb7b4c4d90f5f1673abead4
KumaBar <albermangutov@yandex.ru>
2021-10-11 12:32:36 +0300

    создал PopupList

---
+++ src/components/Popup/PopupList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div :class="{ pList: true, lock: getNumber === getSelectedItems.length }">
+    <PopupItem v-for="item in getPopupItems" :key="item.id" :item="item" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import PopupItem from "./PopupItem.vue";
+export default {
+  name: "PopupList",
+  computed: {
+    ...mapGetters(["getPopupItems", "getNumber", "getSelectedItems"]),
+  },
+  components: {
+    PopupItem,
+  },
+};
+</script>
+
+
+
+<style>
+.pList {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+}
+.lock {
+  opacity: 0.5;
+}
+@media (max-width: 640px) {
+  .pList {
+    justify-content: space-between;
+    flex-wrap:no-wrap;
+  }
+  .pItem{
+    padding:5px;
+    margin:10px;
+  }
+  .p-item-title{
+    width:100%;
+    justify-content: space-between;
+    font-size:12px;
+  }
+}
+@media (max-width:400px){
+ .pItem{
+    padding:5px;
+    margin:0px;
+  }
+}
+</style>
