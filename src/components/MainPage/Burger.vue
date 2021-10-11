c777fc7d2e665c7d3f10ccd5e5fccdeaf8335432
KumaBar <albermangutov@yandex.ru>
2021-10-11 12:24:46 +0300

    создал компонент Burger

---
+++ src/components/MainPage/Burger.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :class="{ burger: true, active: getBurgerOpen }">
+    <ul class="burger-menu">
+      <li class="burger-item" @click="scroll('.section-recommendation')">
+        Рекомендуем
+      </li>
+      <li class="burger-item" @click="scroll('.section-benefit')">Выгода</li>
+      <li class="burger-item" @click="scroll('.section-rolls')">
+        Роллы и суши
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import secrollToElement from "scroll-to-element";
+import { mapGetters } from "vuex";
+export default {
+  name: "Burger",
+  computed: {
+    ...mapGetters(["getBurgerOpen"]),
+  },
+  methods: {
+    scroll(name) {
+      secrollToElement(name, {
+        offset: 0,
+        duration: 500,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.burger {
+  position: fixed;
+  text-align:center;
+  top:50px;
+  
+  right:0;
+  background: white;
+box-shadow: 0px 11px 8px -4px rgba(34, 60, 80, 0.2);
+
+  z-index:2;
+ max-height:0;
+ transition: 0.2s;
+  max-width:260px;
+  width:100%;
+  overflow:hidden;
+}
+.burger.active {
+  max-height:300px;
+}
+.burger-item {
+  padding: 15px 10px;
+  font-size: 20px;
+  cursor:pointer;
+}
+@media(max-width:640px){
+  .burger{
+    top:30px;
+  }
+}
+</style>
