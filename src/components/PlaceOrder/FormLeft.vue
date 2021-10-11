647aa37b2340541050ce13afc61efd3fc8bf0fb8
KumaBar <albermangutov@yandex.ru>
2021-10-11 12:31:15 +0300

    сверстал страницу PlaceOrder

---
+++ src/components/PlaceOrder/FormLeft.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="form-left">
+    <div class="form-left-title">Оформление заказа</div>
+    <div class="form-left-phone">
+      <div class="form-left-phone-title">Ваш номер телефона</div>
+      <div class="form-left-phone-input">
+        <input type="text" />
+      </div>
+    </div>
+    <div class="form-left-delivery">
+      <div class="form-left-delivery-title">Доставка</div>
+      <div class="form-left-delivery-block">
+        <input
+          class="form-left-delivery-sreet"
+          type="text"
+          placeholder="Улица"
+        />
+        <input class="form-left-delivery-home" type="text" placeholder="Дом" />
+        <input
+          class="form-left-delivery-apartment"
+          type="text"
+          placeholder="Кв, офис"
+        />
+        <input
+          class="form-left-delivery-entrance"
+          type="text"
+          placeholder="Подъезд"
+        />
+        <input
+          class="form-left-delivery-floor"
+          type="text"
+          placeholder="Этаж"
+        />
+      </div>
+      <div class="form-left-pay-price">Итого к оплате: {{ getTotalPrice }} ₽</div>
+      <button class='form-left-submit'>Оформить заказ</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "FormLeft",
+  computed: {
+    ...mapGetters(["getTotalPrice"]),
+  },
+};
+</script>
+
+<style>
+.form-left {
+  padding-left: 10px;
+}
+.form-left-title {
+  font-size: 40px;
+  padding-left: 10px;
+  margin-bottom: 20px;
+  padding-top:70px;
+}
+.form-left-phone-title,
+.form-left-delivery-title,
+.form-left-pay-title {
+  font-size: 20px;
+  margin-bottom: 60px;
+  margin-top: 20px;
+}
+.form-left-phone,
+.form-left-delivery,
+.form-left-pay {
+  padding-left: 20px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+}
+.form-left input {
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  margin: 10px;
+  padding: 10px;
+  border-radius: 10px;
+  font-size: 16px;
+  transition: 0.2s;
+}
+.form-left input:hover {
+  border-color: chartreuse;
+}
+
+.form-left-pay-price {
+  color: grey;
+  font-size: 20px;
+  margin-top:20px;
+  margin-bottom: 30px;
+}
+
+.form-left-submit{
+    background:chartreuse;
+    color:white;
+    border-radius: 10px;
+    font-size:20px;
+    padding:20px;
+     margin-bottom: 30px;
+}
+</style>
---
+++ src/components/PlaceOrder/FormRight.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="form-right">
+    <div class="form-right-title">Оформление заказа</div>
+    <div class="form-right-order-title">Ваш заказ</div>
+    <div class="form-right-items">
+      <div
+        class="form-right-item"
+        v-for="item in getOrderedItems"
+        :key="item.id"
+      >
+        <div class="form-right-item-title">{{ item.title }}</div>
+        <div class="form-right-block">
+          <div class="form-right-item-number">{{ item.number }} шт.</div>
+          <div class="form-right-item-price">{{ item.price }} ₽</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "FormRIght",
+  computed: {
+    ...mapGetters(["getOrderedItems"]),
+  },
+};
+</script>
+
+<style>
+.form-right {
+  position: fixed;
+  right: 0;
+  margin-right: 10px;
+  margin-left: 10px;
+  padding-right: 10px;
+  padding-left: 10px;
+  margin-top: 80px;
+  padding-top: 10px;
+  border-radius: 20px;
+  box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
+}
+.form-right-title {
+  font-size: 40px;
+  margin-bottom: 40px;
+  display: none;
+}
+.form-right-items {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.form-right-item {
+  display: flex;
+  justify-content: space-between;
+  width: 400px;
+  font-size: 16px;
+  margin-bottom: 20px;
+}
+.form-right-item-title {
+  max-width: 200px;
+}
+.form-right-block {
+  display: flex;
+}
+.form-right-item-number {
+  padding-right: 10px;
+}
+.form-right-item-price{
+  font-weight:bold;
+}
+.form-right-order-title {
+  font-size: 20px;
+  margin-bottom: 20px;
+  margin-right: 20px;
+}
+@media (max-width: 400px) {
+  .form-right-item-number {
+    padding-right: 5px;
+  }
+  .form-right-item-price {
+    font-size: 12px;
+  }
+  .form-right-item-title,
+  .form-right-item-number {
+    font-size: 12px;
+  }
+}
+</style>
