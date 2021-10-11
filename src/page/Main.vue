ac6d0eff8e99bda86730c5a1ae2f61cce867e9fc
KumaBar <albermangutov@yandex.ru>
2021-10-11 12:34:13 +0300

    создал компонент Main

---
+++ src/page/Main.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="{main:true,pOpen:getOpenPopup}">
+    <Popup />
+    <Navigation />
+    <Burger />
+    <List />
+  </div>
+</template>
+
+<script>
+import Navigation from "../components/MainPage/Navigation";
+import Burger from "../components/MainPage/Burger";
+import List from "../components/MainPage/List";
+import Popup from "../components/Popup/Popup";
+import { mapGetters } from 'vuex';
+export default {
+  name: "Main",
+  components: {
+    Navigation,
+    Burger,
+    List,
+    Popup,
+  },
+  computed:{
+    ...mapGetters(['getOpenPopup'])
+  }
+};
+</script>
+
+<style>
+
+</style>
