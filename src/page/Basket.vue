7e748e865c93d5c11609008ee35456457182ea25
KumaBar <albermangutov@yandex.ru>
2021-10-11 12:34:32 +0300

    создал компонент Basktet

---
+++ src/page/Basket.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="order">
+    <Basket />
+  </div>
+</template>
+
+<script>
+import Basket from "../components/BasketPage/Basket.vue";
+export default {
+  name: "Order",
+  components: {
+    Basket,
+  },
+};
+</script>
+
